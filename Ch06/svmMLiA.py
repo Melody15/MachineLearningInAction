ba3e4c52d4d3e0a4de3e1143314eb942c0d6cd3b
Melody15 <zhuai384177509@qq.com>
2017-12-11 16:06:06 +0800

    update

--- Ch06/svmMLiA.py
+++ Ch06/svmMLiA.py
@@ -202,5 +202,112 @@ def clacWs(alphas, dataArr, classLabels):
         w += multiply(alphas[i]*labelMat[i],X[i,:].T)
     return w
 
+'''
 ws = clacWs(alphas, dataArr, labelArr)
 print(ws)
+'''
+#---------------------------------kernel------------------------------
+
+#核转换函数 kTup元组给出核函数的信息
+def kernelTrans(X, A, kTup):
+    m, n = shape(X)
+    K = mat(zeros((m,1)))
+    if kTup[0] == 'lin':
+        K = X*A.T
+    elif kTup[0] == 'rbf':
+        for j in range(m):
+            deltaRow = X[j,:] - A
+            K[j] = deltaRow*deltaRow.T
+        #元素间的除法
+        K = exp(K/(-1*kTup[1]**2))
+    else:
+        raise NameError('Houston We Have a Problem -- That Kernel is not recognized')
+    return K
+
+class optStruct:
+    def __init__(self, dataMatIn, classLabels, C, toler, kTup):
+        self.X = dataMatIn
+        self.labelMat = classLabels
+        self.C = C
+        self.tol = toler
+        self.m = shape(dataMatIn)[0]
+        self.alphas = mat(zeros((self.m, 1)))
+        self.b = 0
+        self.eCache = mat(zeros((self.m, 2)))
+        self.K = mat(zeros((self.m, self.m)))
+        for i in range(self.m):
+            self.K[:,i] = kernelTrans(self.X, self.X[i,:], kTup)
+
+def calcEk(oS, k):
+    fXk = float(multiply(oS.alphas, oS.labelMat).T*oS.K[:,k] + oS.b
+    Ek = fXk - float(oS.labelMat[k])
+    return Ek
+
+def innerL(i, oS):
+    Ei = calcEk(oS, i)
+    if ((oS.labelMat[i]*Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or ((oS.labelMat[i]*Ei > oS.tol) and (oS.alphas[i] > 0)):
+        j, Ej = selectJ(i, oS, Ei)
+        alphaIold = oS.alphas[i].copy()
+        alphaJold = oS.alphas[j].copy()
+        if oS.labelMat[i] != oS.labelMat[j]:
+            L = max(0, oS.alphas[j] - oS.alphas[i])
+            H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
+        else:
+            L = max(0, oS.alphas[j] + oS.alphas[i] - oS.C)
+            H = min(oS.C, oS.alphas[j] + oS.alphas[i])
+        if L == H:
+            print("L == H")
+            return 0
+        eta = 2.0*oS.K[i,j] - oS.K[i,i] - oS.K[j,j]
+        if eta >= 0:
+            print("eta >= 0")
+            return 0
+        oS.alphas[j] -= oS.labelMat[j]*(Ei - Ej)/eta
+        oS.alphas[j] = clipAlpha(oS.alphas[j], H, L)
+        updateEk(oS, j)
+        if abs(oS.alphas[j] - alphaJold) < 0.00001:
+            print("j not moving enough")
+            return 0
+        oS.alphas[i] += oS.labelMat[j]*oS.labelMat[i]*(alphaJold - oS.alphas[j])
+        updateEk(oS, j)
+        b1 = oS.b - Ei - oS.labelMat[i]*(oS.alphas[i] - alphaIold)*oS.K[i,i] - oS.labelMat[j]*(oS.alphas[j] - alphaJold)*oS.K[i,j]
+        b2 = oS.b - Ei - oS.labelMat[i]*(oS.alphas[i] - alphaIold)*oS.K[i,j] - oS.labelMat[j]*(oS.alphas[j] - alphaJold)*oS.K[j,j]
+        if 0 < oS.alphas[i] and oS.C > oS.alphas[j]:
+            oS.b = b1
+        elif 0 < oS.alphas[j] and oS.C > oS.alphas[j]:
+            oS.b = b2
+        else:
+            oS.b = (b1 + b2)/2.0
+        return 1
+    else:
+        return 0
+
+#利用核函数进行分类的径向基测试函数
+def testRbf(k1 = 1.3):
+    dataArr, labelArr = loadDataSet('testSetRBF.txt')
+    b, alphas = smoP(dataArr, labelArr, 200, 0.0001, 10000, ('rbf', k1))
+    datMat = mat(dataArr)
+    labelMat = mat(labelArr).transpose()
+    svInd = nonzero(alphas.A>0)[0]
+    sVs = datMat[svInd]
+    labelSV = labelMat[svInd]
+    print("there are %d Support Vectors" % shape(sVs)[0])
+    m, n = shape(datMat)
+    errorCount = 0
+    for i in range(m):
+        kernelEval = kernelTrans(sVs, datMat[i,:], ('rbf', k1))
+        predict = kernelEval.T * multiply(labelSV, alphas[svInd]) + b
+        if sign(predict) != sign(labelArr[i]):
+            errorCount += 1
+    print("the training error rate is %f" % (float(errorCount)/m))
+    dataArr, labelArr = loadDataSet('testSetRBF2.txt')
+    errorCount = 0
+    datMat = mat(dataArr)
+    labelMat = mat(labelArr).transpose()
+    m, n = shape(datMat)
+    for i in range(m):
+        kernelEval - kernelTrans(sVs, datMat[i,:], ('rbf', k1))
+        predict = kernelEval.T * multiply(labelSV, alphas[svInd]) + b
+        if sign(predict) != sign(labelArr[i]):
+            errorCount += 1
+    print("the test error rate is: %f" % (float(errorCount)/m))
